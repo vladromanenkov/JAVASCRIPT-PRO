3577b1eeeeb2845302936d231845a70bda4ecf35
vladromanenkov <vladromanenkov@yandex.ru>
2020-07-23 07:47:16 +0300

    lesson2
    1. Добавьте пустые классы для корзины товаров и элемента корзины товаров. Продумайте, какие методы понадобятся для работы с этими сущностями.
    2. Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров.
    
    С бургером не разобрался, да и тут не совсем все понял, постараюсь разобраться

--- js/main.js
+++ js/main.js
@@ -1,4 +1,105 @@
-const products = [
+//ES 6
+class ProductList {
+    #privateProp;
+
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this.#privateProp = '123';
+
+        this.fetchProducts();
+        this.render();
+    }
+
+    get prop() {
+        return this.#privateProp;
+    }
+
+    set prop(value) {
+        this.#privateProp = value;
+    }
+
+    fetchProducts() {
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 20000, img: './image/notebook.jpg'},
+            {id: 2, title: 'Mouse', price: 1500, img: './image/mouse.jpg'},
+            {id: 3, title: 'Keyboard', price: 5000, img: './image/keyboard.jpg'},
+            {id: 4, title: 'Gamepad', price: 4500, img: './image/geympad.jpg'},
+        ];
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+
+        for (let product of this.goods) {
+            const productOdject = new ProductItem(product);
+
+            this.allProducts.push(productOdject);
+            block.insertAdjacentHTML('beforeend', productOdject.render());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product) {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = product.img;
+    }
+
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
+                    <img src="${this.img}" height="120px" alt="Some img">
+                    <div class="desc">
+                        <h3>${this.title}</h3>
+                        <p>${this.price} \u20bd</p>
+                        <button class="by-btn">Купить</button>
+                    </div>
+                </div>`;
+    }
+}
+
+const list = new ProductList();
+
+//1.Добавьте пустые классы для Корзины товаров и Элемента корзины товаров. 
+//Продумайте, какие методы понадобятся для работы с этими сущностями.
+
+class Bag {
+    constructor() {
+
+    }
+    // метод для очистки корзины
+    clearAll() {
+
+    }
+    //метод подсчета суммы купленных товаров
+    total(){
+        let totalPrice=0;
+        this.goods.forEach(item => totalPrice += item.price);
+      return totalPrice;
+    }
+
+    //метод оплаты товаров
+    pay(){
+
+    }
+}
+
+class BagItem {
+    constructor() {
+
+    }
+    //удаляет один элемент из корзины
+    deleteItem(){
+
+    }
+}
+
+
+//ES5
+/* const products = [
     {id: 1, title: 'Notebook', price: 20000, img: './image/notebook.jpg'},
     {id: 2, title: 'Mouse', price: 1500, img: './image/mouse.jpg'},
     {id: 3, title: 'Keyboard', price: 5000, img: './image/keyboard.jpg'},
@@ -23,3 +124,4 @@ const renderProducts = list => {
 };
 
 renderProducts(products);
+ */
